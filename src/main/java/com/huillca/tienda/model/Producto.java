3459e9e254bcc219d85db19cd4c177c22a9dc25e
Italo H <yhon.huillca@tecsup.edu.pe>
2025-04-24 14:42:10 -0500

    Corrigiendo clase Producto

--- src/main/java/com/huillca/tienda/model/Producto.java
+++ src/main/java/com/huillca/tienda/model/Producto.java
@@ -5,10 +5,44 @@
 
 package com.huillca.tienda.model;
 
-/**
- *
- * @author xdork
- */
-public class Producto {
 
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+
+    // Constructor vacío
+    public Producto() {}
+
+    // Constructor con parámetros
+    public Producto(int id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
